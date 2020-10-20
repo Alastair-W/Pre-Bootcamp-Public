31fea77e12a81b1a5fac985cbe58508520296295
RowdyOnline <rowdy14@gmail.com>
2020-10-20 11:56:57 -0700

    Random map gen on Ninjaman

--- new_test.js
+++ new_test.js
@@ -8,3 +8,61 @@ function displayIfChildIsAbleToRideTheRollerCoaster(childHeight) {
 }
 
 displayIfChildIsAbleToRideTheRollerCoaster(childHeight)
+
+var x = 8;
+var y = 10;
+
+function getRndInteger(x, y) {
+    return Math.floor(Math.random() * (y - x) ) + 1;
+    
+}
+
+//console.log(getRndInteger(x,y));
+
+var world = [];
+
+var worldDict = {
+    0: 'wall',
+    1: 'blank',
+    2: 'sushi',
+    3: 'onigiri'
+}
+//console.log(Object.keys(worldDict).length);
+
+var pointsDict = {
+    0: 0,
+    1: 0,
+    2: 10,
+    3: 5
+}
+
+function createWorld() {
+    
+    var max_rows = Math.floor(Math.random() * 10) + 5;
+    var max_cols = Math.floor(Math.random() * 10) + 5;
+    var data = [];
+    for (var rows = 0; rows < max_rows; rows++) {
+        world.push([]);
+        for (var i = 0; i < max_cols; i++) {
+            data = Math.floor(Math.random() * (Object.keys(worldDict).length -1));
+            data[0] = 0;
+            world[rows].push(data);
+        }   
+    
+    }
+var maxRowsIndex = max_rows -1;
+for (var t = 0; t < max_cols;t++) {
+    world[0][t] = 0; 
+    world[maxRowsIndex][t] = 0;
+}
+var maxColsIndex = max_cols -1;
+for (var u = 0; u < max_rows; u++) {
+    world[u][0] = 0;
+    world[u][maxColsIndex] = 0;
+}
+
+console.log("Rows: "+max_rows);
+console.log("Cols: "+max_cols);
+console.log(world);
+}
+createWorld()
---
+++ ninjaman/readme.txt
@@ -0,0 +1,23 @@
+FUNCTIONS
+MAP GEN
+Randomnly generate a Ninjaman map within a max size
+All map rows must be the same length as each other
+All map columns must be the same length as each other
+Map will be consisted off walls and spaces
+Walls must always occupy the edge of the mnap (horizontal and vertical)
+In the spaces there will be a randomly generated sushi item
+The map must be generated in a way that the each space must have least one other space connected/adjacent to it
+This connection/adjacency requirement is not met by a diagnol connection
+
+MOVEMENT
+Ninjaman must be placed on the map in a space and can be moved around the spaces on the map using up, down, left and right
+
+SCORING
+Every time the the a sushi is eaten the player will increase their score
+The scoring needs to be configurable for each sushi item
+
+HOSTILES
+There will also be Hostiles following trying to get Ninjaman
+This may be done by the Hostile randomnly navigating the spaces on the map until it occupies the same space as Ninjaman at which point the game is over
+
+
