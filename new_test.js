8d604b3b28b1536b7c5496d900a0600afde3843a
RowdyOnline <rowdy14@gmail.com>
2020-10-25 15:13:38 -0700

    Final(ish) still need to make ghost move and make sure every map is connected

--- new_test.js
+++ new_test.js
@@ -44,7 +44,7 @@ function createWorld() {
     for (var rows = 0; rows < max_rows; rows++) {
         world.push([]);
         for (var i = 0; i < max_cols; i++) {
-            data = Math.floor(Math.random() * (Object.keys(worldDict).length -1));
+            data = Math.floor(Math.random() * (Object.keys(worldDict).length));
             data[0] = 0;
             world[rows].push(data);
         }   
@@ -61,8 +61,110 @@ for (var u = 0; u < max_rows; u++) {
     world[u][maxColsIndex] = 0;
 }
 
+
 console.log("Rows: "+max_rows);
 console.log("Cols: "+max_cols);
 console.log(world);
+
 }
-createWorld()
+//createWorld()
+
+
+
+//test
+var world = [
+    [0,0,0,0],
+    [0,0,1,0],
+    [0,0,0,0],
+    [0,2,1,0],
+    [0,0,0,0]
+];
+
+//Checks any cell and, if less than one of the adjacent cells is not a wall it will replace one of the wall items with an accesible item like a sushi or space
+//Need to loop through every cell in the randomnly generated map, except the borders, and run this logic
+//Once the function is run it should write the new cell value/item to the map
+//This code doesn't work!!!!
+
+// for (var worldRow = 1; worldRow < world.length-1; worldRow++) {
+    
+//     var blockArray = [];
+//     for (var worldCol = 1; worldCol < world[worldRow].length-1; worldCol++) {
+//         var sum = 0;
+//         blockArray.push(world[worldRow][worldCol]);
+//         cellContact = [world[worldRow][worldCol-1], world[worldRow][worldCol+1], world[worldRow-1][worldCol], world[worldRow+1][worldCol]];
+//         console.log(cellContact);
+//         console.log(world[worldRow][worldCol]);
+//         for (var b = 0; b < cellContact.length; b++) {
+//             if (cellContact[b] == 0) {
+//                 sum += 1;
+//                 console.log(sum);
+//                 if (sum == 3) {
+//                     for (var num = 0; num < cellContact.length; num++) {
+//                         counterNew = 0;
+//                         if (cellContact[num] == 0 && counterNew == 2) { //LEFT OF CELL
+//                             world[worldRow][worldCol-1] = Math.floor(Math.random() * (Object.keys(worldDict).length));
+//                             counterNew += 1;
+//                         }
+//                         else if (cellContact[num] == 0 && counterNew == 2) { //RIGHT OF CELL
+//                             world[worldRow][worldCol+1] = Math.floor(Math.random() * (Object.keys(worldDict).length));
+//                             counterNew += 1;
+//                         }
+//                         else if (cellContact[num] == 0 && counterNew == 2) { //UP OF CELL
+//                             world[worldRow-1][worldCol] = Math.floor(Math.random() * (Object.keys(worldDict).length));
+//                             counterNew += 1;
+//                         }
+//                         else if (cellContact[num] == 0 && counterNew == 2) { //DOWN OF CELL
+//                             world[worldRow+1][worldCol] = Math.floor(Math.random() * (Object.keys(worldDict).length));
+//                             counterNew += 1;
+//                         }
+//                     }
+//                 }        
+//             }            
+//         }
+//     }
+// }
+
+//onsole.log(world);
+
+var num = 0;
+var interval = setInterval('count()', 1000);
+function count() {
+    if ( Math.random ( ) > .5 ) {
+    num = "Woo!";
+    }
+    else {
+    num = "Yay!";
+    }
+console.log(num);
+setTimeout(num = "", 500);
+}
+
+
+// var speed = setInterval(ghostMove(), 1000);
+
+//         function ghostMove() {
+//             var moveScore = Math.floor(Math.random() * 3);
+//             console.log(moveScore);
+//             if (moveScore == 0 && world[ghostLocation.y][ghostLocation.x-1] != 0) { //LEFT
+//                 ghostLocation.x--;
+//             }
+//             else if (moveScore == 1 && world[ghostLocation.y][ghostLocation.x+1] != 0) { //RIGHT
+//                 ghostLocation.x--;
+//             }
+//             else if (moveScore == 2 && world[ghostLocation.y+1][ghostLocation.x] != 0) { //DOWN
+//                 ghostLocation.x--;
+//             }
+//             else if (moveScore == 3 && world[ghostLocation.y-1][ghostLocation.x] != 0) { //UP
+//                 ghostLocation.x--;
+//             }
+//             else {
+//                 ghostMove()
+//             }
+//         }
+
+// ghostMove()
+
+// if (ghostLocation == ninjamanLocation) {
+//     document.getElementById('gameOver').innerHTML = "GAME OVER";
+//     break;
+// }
